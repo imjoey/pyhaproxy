bb02127cccffbc2b8bbdacddf664a3b9e13a4067
imjoey <majunjiev@qq.com>
2015-11-26 11:16:03 +0800

    initital commit for unit testing

---
+++ pyhaproxy/test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class TestParse(object):
+
+    @classmethod
+    def setup_class(cls):
+        pass
+
+    @classmethod
+    def teardown_class(cls):
+        pass
+
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass

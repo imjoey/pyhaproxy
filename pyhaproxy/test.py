0b4362603eebd6e7fc30ef16b77f7a968699621e
imjoey <majunjiev@qq.com>
2015-11-26 11:21:58 +0800

    move some print functions from parse.py to test.py

--- pyhaproxy/test.py
+++ pyhaproxy/test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import parse
+
 
 class TestParse(object):
 
@@ -13,7 +15,24 @@ class TestParse(object):
         pass
 
     def setup(self):
-        pass
+        self.parser = parse.Parser('haproxy.cfg')
+        self.configration = self.parser.build_configration()
 
     def teardown(self):
         pass
+
+    def test_parse_global_section(self):
+        print self.configration.globall.configs
+        print '-' * 30
+        print self.configration.globall.options
+
+    def test_parse_frontend_section(self):
+        for frontend in self.configration.frontends:
+            print frontend.name, frontend.host, frontend.port
+            print frontend.configs
+            print frontend.options
+            print '-' * 30
+
+
+if __name__ == '__main__':
+    main()

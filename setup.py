357a40fa2ace8911210e770e1fbe67bad43569a5
iterjpnic <majunjiev@gmail.com>
2016-04-15 09:49:30 +0800

    fix #5, then change version to 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyhaproxy',
-    version='0.2.0',
+    version='0.2.1',
     keywords=('haproxy', 'parse'),
     description='A Python library to parse haproxy configuration file',
     license='MIT License',

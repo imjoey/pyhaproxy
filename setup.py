8178d8b6be7f4398985e21823c4b59497f89f142
imjoey <majunjiev@gmail.com>
2017-08-29 16:22:39 +0800

    set version to 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyhaproxy',
-    version='0.2.4',
+    version='0.3.0',
     keywords=('haproxy', 'parse'),
     description='A Python library to parse haproxy configuration file',
     license='MIT License',

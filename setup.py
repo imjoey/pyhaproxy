f6ccd1c20e12c5889f0e77d3be7743a03dceb14a
imjoey <majunjiev@gmail.com>
2017-09-28 20:39:35 +0800

    Move version to 0.3.6.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyhaproxy',
-    version='0.3.4',
+    version='0.3.6',
     keywords=('haproxy', 'parse'),
     description='A Python library to parse haproxy configuration file',
     license='MIT License',

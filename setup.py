4f0cbf4c9921f4801bf67040448d9f8f4c654057
Joey <majunjiev@gmail.com>
2019-12-06 17:17:31 +0800

    Bump version to 0.3.7 (#28)
    Signed-off-by: imjoey <majunjiev@gmail.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyhaproxy',
-    version='0.3.6',
+    version='0.3.7',
     keywords=('haproxy', 'parse'),
     description='A Python library to parse haproxy configuration file',
     license='MIT License',
